73fd96ee651ae31cfd0133c4c73cc52e0d48e75c
SmutnyJan <57211415+SmutnyJan@users.noreply.github.com>
2022-04-12 19:57:30 +0200

    Aktualizovat pxt.json, main.blocks, main.ts, balancing.ts

--- balancing.ts
+++ balancing.ts
@@ -1,39 +1,31 @@
 enum Difficulty {
-    Jednoducha = 100,
-    Stredni = 50,
-    Tezka = 25,
+    //% block="Jednoduchá"
+    Easy = 100,
+    //% block="Normální"
+    Normal = 50,
+    //% block="Těžká"
+    Hard = 25,
 }
 
 //% weight=100 color=#1d1f1d icon="\uf24e" block="Balancování"
 namespace balancing {
 
-    let boundary = 30
-    let isGameInProgress = false
+    let boundary = Difficulty.Normal
+    let methodLock = false
+    let currentCoordinates: number[] = []
+
     let x = 0
     let y = 0
     let previousX = 0
     let previousY = 0
-    
+
     /**
-    * Spustí hru s obtížností
+    * Nastaví obtížnost hry
     * @difficulty Obtížnost hry
-    */  
-    //% block="Spusť hru s obtížností %difficulty"
-    export function playGame(difficulty: Difficulty): void {
+    */
+    //% block="Nastav obtížnost na %difficulty"
+    export function setDifficulty(difficulty: Difficulty): void {
         boundary = difficulty
-        basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-
-        x = Math.floor(Math.floor(Math.map(input.rotation(Rotation.Roll), -180 + (180 - boundary), 180 - (180 - boundary), -1, 5) / 0.8))
-        y = Math.floor(Math.floor(Math.map(input.rotation(Rotation.Pitch), -180 + (180 - boundary), 180 - (180 - boundary), -1, 5) / 0.8))
-        led.plot(x, y)    
-        isGameInProgress = true
-
     }
 
     /**
@@ -41,17 +33,22 @@ namespace balancing {
     */
     //% block="Aktualizuj"
     export function tick(): void {
-        if (isGameInProgress) {
+        if (x != previousX || y != previousY) {
+            led.unplot(previousX, previousY)
+            led.plot(x, y)
             previousX = x
             previousY = y
-            x = Math.floor(Math.map(input.rotation(Rotation.Roll), -180 + (180 - boundary), 180 - (180 - boundary), -1, 5) / 0.8)
-            y = Math.floor(Math.map(input.rotation(Rotation.Pitch), -180 + (180 - boundary), 180 - (180 - boundary), -1, 5) / 0.8)
-            if (x != previousX || y != previousY) {
-                led.unplot(previousX, previousY)
-                led.plot(x, y)
-            }
-
         }
+    }
+
+    /**
+    * Vrátí aktuální souřadnice
+    */
+    //% block="Souřadnice"
+    export function coordinates(): number[] {
+        currentCoordinates[0] = x
+        currentCoordinates[1] = y
+        return currentCoordinates
     }
 
     /**
@@ -64,14 +61,15 @@ namespace balancing {
 
         control.onEvent(eventId, 0, function () {
             control.inBackground(() => {
+                methodLock = true
                 action()
+                methodLock = false
             })
         })
 
         control.inBackground(() => {
             while (true) {
-                if (isGameInProgress && (x == 0 || x == 4 || y == 0 || y == 4)) {
-                    isGameInProgress = false
+                if (methodLock == false && (x == 0 || x == 4 || y == 0 || y == 4)) {
                     control.raiseEvent(eventId, 1)
                 }
                 basic.pause(20)
@@ -79,9 +77,11 @@ namespace balancing {
         })
     }
 
-
-
-
-
-
+    control.inBackground(() => {
+        while (true) {
+            x = Math.floor(Math.map(input.rotation(Rotation.Roll), -180 + (180 - boundary), 180 - (180 - boundary), -1, 5) / 0.8)
+            y = Math.floor(Math.map(input.rotation(Rotation.Pitch), -180 + (180 - boundary), 180 - (180 - boundary), -1, 5) / 0.8)
+            basic.pause(20)
+        }
+    })
 }
--- main.ts
+++ main.ts
@@ -1,9 +1,42 @@
-balancing.onEggDrop(function () {
-    soundExpression.giggle.play()
-})
-input.onButtonPressed(Button.B, function () {
-    balancing.playGame(Difficulty.Tezka)
+let jeHraSpustena = false
+let souradnice: number[] = []
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    jeHraSpustena = !(jeHraSpustena)
 })
 basic.forever(function () {
-    balancing.tick()
+    if (jeHraSpustena) {
+        balancing.tick()
+        souradnice = balancing.coordinates()
+    }
+    if (jeHraSpustena && (souradnice[0] == 0 || souradnice[0] == 4 || souradnice[1] == 0 || souradnice[1] == 4)) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            # # # # #
+            # . . . #
+            `)
+        music.startMelody(music.builtInMelody(Melodies.Ringtone), MelodyOptions.Once)
+        jeHraSpustena = false
+    }
 })
+/*
+balancing.onEggDrop(function () {
+    if (jeHraSpustena) {
+        jeHraSpustena = false
+        music.playTone(262, music.beat(BeatFraction.Whole))
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    jeHraSpustena = !(jeHraSpustena)
+})
+let jeHraSpustena = false
+balancing.setDifficulty(Difficulty.Easy)
+basic.forever(function () {
+    if (jeHraSpustena) {
+        balancing.tick()
+    }
+})
+
+*/
