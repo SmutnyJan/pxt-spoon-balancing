39b4e7ab67788ba501e6d64cb11d229b9ebcdee8
SmutnyJan <57211415+SmutnyJan@users.noreply.github.com>
2022-04-14 18:29:57 +0200

    Aktualizovat pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,16 @@
+balancing.onEggDrop(function () {
+    if (jeHraSpustena) {
+        jeHraSpustena = false
+        music.playTone(262, music.beat(BeatFraction.Whole))
+    }
+})
 input.onButtonPressed(Button.A, function () {
-    basic.clearScreen()
     jeHraSpustena = !(jeHraSpustena)
 })
-let souradnice: number[] = []
 let jeHraSpustena = false
-balancing.setDifficulty(Difficulty.Normal)
+balancing.setDifficulty(Difficulty.Easy)
 basic.forever(function () {
     if (jeHraSpustena) {
         balancing.tick()
-        souradnice = balancing.coordinates()
-    }
-    if (jeHraSpustena && (souradnice[0] == 0 || souradnice[0] == 4 || souradnice[1] == 0 || souradnice[1] == 4)) {
-        basic.showLeds(`
-            # . . . #
-            . # . # .
-            . . # . .
-            # # # # #
-            # . . . #
-            `)
-        music.startMelody(music.builtInMelody(Melodies.Ringtone), MelodyOptions.Once)
-        jeHraSpustena = false
     }
 })
